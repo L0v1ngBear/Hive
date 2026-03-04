1c55d0c436bf321c9f1118263c869b34b3dd6acb
曹立峰 <caolf56397@hundsun.com>
2026-03-04 10:53:59 +0800

    init后端项目

---
+++ server/src/main/java/my/hive_back/HiveBackApplication.java
@@ -0,0 +1,13 @@
+package my.hive_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiveBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiveBackApplication.class, args);
+	}
+
+}
